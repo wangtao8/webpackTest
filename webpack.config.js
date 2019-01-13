7a40d1efdc5220b1e4acaddbd075d71e52dff395
wangtao <602697966@qq.com>
2019-01-13 19:20:13 +0800

    测试

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 let path = require('path');
 
 module.exports = {
-    entry: ['./new.js'],
-    output: {
-        path: path.join(__dirname, './'),
-        filename: 'hebing.js'
+    entry: ['./news.js'],//入口文件 将要被打包的文件路径
+    output: {//打包后的文件
+        path: path.join(__dirname, './'),//打包后的文件路径
+        filename: 'hebing.js'//打包后的文件名
     },
-    devtool: 'source-map',
+    devtool: 'source-map',//生成map
 };
